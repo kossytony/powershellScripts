f78e17edc5a61fe60f055d343bb2f4c7143e0a69
kossytony <kossytony@gmail.com>
2021-06-18 13:03:13 -0400

    intial commit- Tested working script

---
+++ VMbackUpReport.ps1
@@ -0,0 +1,28 @@
+
+#set az context by putting the subscription ID
+Set-AzContext ""
+
+#get all VMs in the 
+$myVM = Get-azVM 
+
+$missingarray =@()
+$SeriesNo = 0
+
+foreach ($VM in $myVM) {
+
+$backup = Get-AzRecoveryServicesBackupStatus -Name $VM.name -ResourceGroupName $VM.ResourceGroupName -Type AzureVM
+
+
+$seriesNo ++ 
+$myobj = [PSCustomObject]@{
+    SNo = $SeriesNo
+    Name = $VM.name
+    ResourceGroup = $VM.ResourceGroupName
+    backupStatus = $backup.BackedUp
+  
+    
+}
+#$myobj
+$missingarray += $myobj
+}
+$missingarray | export-csv -path "BackupReport.csv" -NoTypeInformation
