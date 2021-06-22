7de7414cfb68da3f2fbab99ee2ef7f0217d69d39
kossytony <kossytony@gmail.com>
2021-06-21 23:25:46 -0400

    added feature to loop through subscriptions

--- AZVMbackUpReport.ps1
+++ AZVMbackUpReport.ps1
@@ -1,44 +1,38 @@
 
-
-
-
-
 $subscriptions = Get-AzSubscription
 
-
-
-foreach ( $subscription in $subscriptions ) {
-
-#set az context by putting the subscription ID
-Set-AzContext ""
-
-#get all VMs in the subscription
-$myVM = Get-azVM 
-
 #empty array to hold report objects
-$missingarray =@()
+$missingarray = @()
 #variable to create serial No
 $SeriesNo = 0
 
-#looping through list of VMs
-foreach ($VM in $myVM) {
+foreach ( $subscription in $subscriptions ) {
 
-$backup = Get-AzRecoveryServicesBackupStatus -Name $VM.name -ResourceGroupName $VM.ResourceGroupName -Type AzureVM
+    #set az context by putting the subscription ID
+    Set-AzContext $subscription.id
 
-#Building Object of the report
-$seriesNo ++ 
-$myobj = [PSCustomObject]@{
-    SNo = $SeriesNo
-    Name = $VM.name
-    ResourceGroup = $VM.ResourceGroupName
-    backupStatus = $backup.BackedUp
+    #get all VMs in the subscription
+    $myVM = Get-azVM 
+
+    #looping through list of VMs
+    foreach ($VM in $myVM) {
+
+        $backup = Get-AzRecoveryServicesBackupStatus -Name $VM.name -ResourceGroupName $VM.ResourceGroupName -Type AzureVM
+
+        #Building Object of the report
+        $seriesNo ++ 
+        $myobj = [PSCustomObject]@{
+            SNo           = $SeriesNo
+            Name          = $VM.name
+            ResourceGroup = $VM.ResourceGroupName
+            backupStatus  = $backup.BackedUp
   
     
-}
+        }
 
-#adding objects to the Array
-$missingarray += $myobj
+        #adding objects to the Array
+        $missingarray += $myobj
+    }
 }
-]
 #Piping array to the export-csv function to create a CSV file.
 $missingarray | export-csv -path "BackupReport.csv" -NoTypeInformation
